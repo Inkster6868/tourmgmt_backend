36e819c9f828f8afbc427b482f4e83250386104c
Tushar <tushardua6868@gmail.com>
2023-02-11 04:28:58 +0530

    final

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app=express(); 
 const port=process.env.PORT || 8000;
 const corsOptions={
-        origin:'https://wayfarewonders.vercel.app/',
+        origin:true,
         credentials:true
 }
 
